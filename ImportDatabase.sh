d648f0fad5f9784b04c4b946080ff8c3555737b0
phannamqt <phannam88@gmail.com>
2018-11-29 00:06:56 +0700

    Update ImportDatabase.sh
    s

--- ImportDatabase.sh
+++ ImportDatabase.sh
@@ -1,14 +1,5 @@
 pkg1=$(rpm -qa| grep MariaDB-server)
 if [[ "$pkg1" == *"MariaDB-server-10.1."*  ]];then
-
-	 pkg2=$(rpm -qa| grep wget)
-	 if [[ "$pkg2" == *"wget"*  ]];then
-	  echo 'wget package... OK!' 
-	 else
-	  echo 'Installing wget...'
-	  yum install -y wget
-	  echo " Done!"
-	 fi
 	 wget https://github.com/phannamqt/Maria_Master-Slave/blob/master/demosql.tar.gz -P /tmp/ && cd /tmp/ && tar zxvf demosql.tar.gz >/dev/null 2>&1
 	 mysql -uroot -p demosql < /tmp/demosql.sql
 	 rm -rf /tmp/demosql.*
